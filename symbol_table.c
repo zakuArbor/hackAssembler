870b52524c4950c487406a856107825884c2b599
Ju Hong Kim <nabimoon1234@gmail.com>
2020-08-09 13:45:47 -0400

    [symbol_table] Initialize symbol table with predefined symbols

--- symbol_table.c
+++ symbol_table.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "symbol_table.h"
 
 char *dec_to_bin(struct symbol_table *symbol) {
@@ -26,9 +27,80 @@ char *dec_to_bin(struct symbol_table *symbol) {
   	return bin;
 }
 
+struct symbol_table *create_sym_entry(char *symbol, int val) {
+	struct symbol_table *entry;
+	if (! ( entry = malloc(sizeof(struct symbol_table)) ) ) {
+		perror("malloc");
+		return NULL;
+	}
+	if (!symbol || strlen(symbol) > SYMBOL_SIZE) {
+		fprintf(stderr, "create_sym_entry: symbol is empty or exceeds over %d charact limit\n", SYMBOL_SIZE);
+		return NULL;
+	}
+
+	strncpy(entry->symbol, symbol, strlen(symbol));
+	entry->symbol[strlen(symbol)] = '\0';
+	entry->val = val;
+	entry->next = NULL;
+
+	return entry;
+}
+
+void destroy_table (struct symbol_table *entry) {
+	if (!entry) {
+		return;
+	}
+	struct symbol_table *next = entry->next;
+	free(entry);
+	destroy_table(next);
+}
+
+void print_sym_table(struct symbol_table *entry) {
+	while (entry) {
+		printf("sym: %-3s\t val: %d\n", entry->symbol, entry->val);
+		entry = entry->next;
+	}
+}
+
+struct symbol_table *init_sym_table() {
+	struct symbol_table *first_entry = create_sym_entry("R0", 0);
+	struct symbol_table *entry = first_entry;
+	int i, n;
+
+	char *symbols[7] = {"SCREEN", "KBD", "SP", "LCL", "ARG", "THIS","THAT"};
+	int values[7] = {16384, 24576, 0, 1, 2, 3, 4};
+
+	char sym[7];
+
+	//Add Register 1-15 to symbol table
+	for(i = 1; i < 16; i++) {
+		if (i < 10 ) {
+			n = 3; //'Rx' + '\0'
+		}
+		else {
+			n = 4; //'Rxx' + '\0'
+		}
+		snprintf(sym, n, "R%d", i);
+		if ( !(entry->next = create_sym_entry(sym, i)) ) {
+			destroy_table(first_entry);
+			return NULL;
+		}
+		entry = entry->next;
+	}
+
+	//Add Screen and Keyboard and special pre-defined symbols
+	for (i = 0; i < 7; i++) {
+		if ( !(entry->next = create_sym_entry(symbols[i], values[i])) ) {
+			destroy_table(first_entry);
+			return NULL;
+		}
+		entry = entry->next;
+	}
+	//print_sym_table(first_entry);
+	return table;
+}
+
 int main () {
-	struct symbol_table s;
-	s.val = 21;
-	printf("\n%s\n", dec_to_bin(&s));
+	init_sym_table();
 	return 0;
 }
--- symbol_table.h
+++ symbol_table.h
@@ -1,12 +1,15 @@
 #include <stdlib.h>
 
 #define BUS_SIZE 16
+#define SYMBOL_SIZE 25
 
 struct symbol_table {
-	char symbol[25];
+	char symbol[SYMBOL_SIZE];
 	int val;
 	struct symbol_table *next;
 };
+
+struct symbol_table *table;
 
 /*
 * Convert decimal to a 15 bit binary number
@@ -15,3 +18,24 @@ struct symbol_table {
 * 	Note: allocates a 16 character string
 */
 char *dec_to_bin(struct symbol_table *symbol);
+
+/*
+* Display all entries in the symbol table
+* @param entry: the first entry in the symbol table
+*/
+void print_sym_table(struct symbol_table *entry)
+
+/*
+* Destroys (frees) all entries in the table
+*
+* @param: an entry of the symbol table
+*/
+void destroy_table (struct symbol_table *entry);
+
+/*
+* Initializes all the pre-defined tables to the symbol table
+*
+* @return: the first entry in the symbol table
+* @error: returns NULL and frees up the symbol table
+*/
+struct symbol_table *init_sym_table();
